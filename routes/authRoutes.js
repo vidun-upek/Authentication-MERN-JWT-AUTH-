e2da00cd97abbecbac5c49c2a264e41619153633
Vidun-Shanuka-Upek <vidun.sh@gmail.com>
2025-11-19 18:34:06 +0530

    remove unwanted code lines

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,3 @@
-// import express from 'express'
-// import { register,login,logout, sendVerifyOtp, verifyEmail,isAuthenticated, sendResetOtp, resetPassword } from '../controllers/authController.js';
-// import userAuth from '../middleware/userAuth.js';
-
-// const authRouter = express.Router();
-
-// authRouter.post('/register', register);
-// authRouter.post('/login', login);
-// authRouter.post('/logout', logout);
-// authRouter.post('/send-verify-otp', userAuth,sendVerifyOtp);
-// authRouter.post('/verify-account',userAuth, verifyEmail);
-// authRouter.post('/is-auth',userAuth, isAuthenticated);
-// authRouter.post('/send-reset-otp', sendResetOtp);
-// authRouter.post('/rest-password', resetPassword);
-
-// export default authRouter;
 
 import express from 'express'
 import { register,login,logout, sendVerifyOtp, verifyEmail,isAuthenticated, sendResetOtp, resetPassword } from '../controllers/authController.js';
