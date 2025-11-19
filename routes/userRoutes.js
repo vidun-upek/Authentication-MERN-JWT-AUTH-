8649bfd0c4385747948d5d89762213cba955167d
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2025-11-19 18:53:18 +0530

    Completely removed all server files from repository

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userAuth from '../middleware/userAuth.js';
-import {getUserDate} from  '../controllers/userController.js'
-
-const userRouter = express.Router();
-
-userRouter.get('/data',userAuth,getUserDate);
-
-export default userRouter;
