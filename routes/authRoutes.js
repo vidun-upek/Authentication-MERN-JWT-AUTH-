fd1d724fa76879bd58514b904c70d1d5e724fb79
Vidun-Shanuka-Upek <vidunshanusocial@gmail.com>
2025-11-19 18:38:45 +0530

    configure final server problems

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,9 +8,9 @@ const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.post('/send-verify-otp', userAuth,sendVerifyOtp); // Protected
-authRouter.post('/verify-account',userAuth, verifyEmail); // Protected
-authRouter.post('/is-auth',userAuth, isAuthenticated); // Protected
+authRouter.post('/send-verify-otp', userAuth,sendVerifyOtp); 
+authRouter.post('/verify-account',userAuth, verifyEmail); 
+authRouter.post('/is-auth',userAuth, isAuthenticated); 
 authRouter.post('/send-reset-otp', sendResetOtp);
 authRouter.post('/rest-password', resetPassword);
 
